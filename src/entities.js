0877e2e2862ac4cb903c90b7ab6769ebf80afbf2
Gaspard4i <gaspard.catry.etu@univ-lille.fr>
2025-03-11 12:03:05 +0100

    refector entities

--- src/entities.js
+++ src/entities.js
@@ -3,7 +3,7 @@ import { canvas } from './canvas.js';
 
 export const stains = [];
 for (let i = 0; i < 20; i++) {
-	const x = Math.floor(Math.random() * canvas.clientWidth-10);
-	const y = Math.floor(Math.random() * canvas.clientHeight-10);
-	stains.push(new Stain(20, x, y, 0, 0));
+	const x = Math.floor(Math.random() * canvas.clientWidth - 10);
+	const y = Math.floor(Math.random() * canvas.clientHeight - 10);
+	stains.push(new Stain(20, x, y));
 }
