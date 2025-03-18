77114edf0b48bf897f187bed87ed72f03f7b2d1c
Florian Gambirasio <florian.gambirasio.etu@univ-lille.fr>
2025-03-18 14:05:13 +0100

    add test for score that up when player grow

--- test/player.test.js
+++ test/player.test.js
@@ -1,6 +1,12 @@
 import assert from 'node:assert/strict';
 import { describe, it, before } from 'node:test';
+import { Player } from '../src/player.js';
 
 describe('Player Module', () => {
-	
+	it('Score should up when player grow', () => {
+		const player = new Player(0, 0, 0, 0, 0, false);
+		assert.equal(player.score, 0);
+		player.grow();
+		assert.equal(player.score, 15);
+	});
 });
